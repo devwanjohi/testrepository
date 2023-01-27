c847bffe15b30cd9998174e0e20ce13b0407953c
Paul Wanjohi <119477356+devwanjohi@users.noreply.github.com>
2023-01-27 23:13:12 +0300

    Update flower4.py

--- flower4.py
+++ flower4.py
@@ -15,7 +15,18 @@ def drawdesign():
     for i in range(10):
         drawcircle(150)
         turtle.right(36)
+        
+def drawcircle(radius):
+    for i in range(10):
+        turtle.circle(radius)
+        radius-=4
+
+def drawdesign():
+    for i in range(10):
+        drawcircle(150)
+        turtle.right(36)        
 
 drawdesign()
+drawcircle()
 turtle.done()
 
